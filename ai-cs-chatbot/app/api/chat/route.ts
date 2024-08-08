40772c36d8b59b0b07ff173e10ef3bf66722ee8f
aahana4 <cclttchopper@gmail.com>
2024-08-08 17:16:01 -0400

    milestone commit 2

--- ai-cs-chatbot/app/api/chat/route.ts
+++ ai-cs-chatbot/app/api/chat/route.ts
@@ -10,13 +10,14 @@ export async function POST(req: Request) {
         if (!process.env.OPENAI_API_KEY){
             return new NextResponse('Missing OpenAI API Key.', { status: 400 })
         }
-
         const {messages} = await req.json()
+
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             stream: true,
             messages
         })
+        
 
         const stream = OpenAIStream(response)
         return new StreamingTextResponse(stream)
--- ai-cs-chatbot/components/chat.tsx
+++ ai-cs-chatbot/components/chat.tsx
@@ -32,7 +32,7 @@ export default function Chat() {
     return (
         <section className = 'text-zinc-700'>
             <div className = 'container flex h-screen flex-col items-center justify-center'>
-                <h1 className = 'font-serif text-2x1 font-medium'></h1>
+                <h1 className = 'font-serif text-2x1 font-medium'>AI Chatbot</h1>
                 <div className = 'mt-4 w-full max-w-lg'>
                     {/* {response container} */}
                     <ScrollArea
